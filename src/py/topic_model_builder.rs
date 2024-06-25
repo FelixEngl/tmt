3006e435623e9adf68a1b50e0ee3a5cd04a0cd91
Felix Engl <felix.engl@hotmail.com>
2024-06-25 19:40:54 +0200

    Make all good

--- src/py/topic_model_builder.rs
+++ src/py/topic_model_builder.rs
@@ -20,13 +20,15 @@ pub struct PyTopicModelBuilder {
 }
 
 impl PyTopicModelBuilder {
-    fn set_probability(&mut self, topic_id: usize, word_id: usize, probability: f64) {
+    fn set_probability_impl(&mut self, topic_id: usize, word_id: usize, probability: f64) {
         if let Some(found) = self.topics.get_mut(topic_id) {
             if let Some(found) = found.get_mut(word_id) {
                 *found = probability;
             } else {
-                let mut result = vec![f64::NAN; self.voc.len()];
-                result.insert(word_id, probability);
+                let target_len = self.voc.len() - found.len();
+                found.reserve(target_len);
+                found.extend(repeat_n(f64::NAN, target_len));
+                found.insert(word_id, probability);
             }
         } else {
             while self.topics.len() <= topic_id {
@@ -38,10 +40,10 @@ impl PyTopicModelBuilder {
         }
     }
 
-    fn set_frequency(&mut self, word_id: usize, frequency: u64) {
+    fn set_frequency_impl(&mut self, word_id: usize, frequency: u64) {
         if let Some(target) = self.used_vocab_frequency.get_mut() {
             if target.len() <= word_id {
-                let to_add = word_id - target.len() + 1;
+                let to_add = word_id - target.len();
                 target.reserve(to_add);
                 target.extend(repeat_n(0, to_add));
             }
@@ -68,7 +70,8 @@ impl PyTopicModelBuilder {
     }
 
     pub fn set_frequency(&mut self, word: String, frequency: u64) {
-        self.set_frequency(self.voc.add_value(word), frequency);
+        let word_id = self.voc.add_value(word);
+        self.set_frequency_impl(word_id, frequency);
     }
 
     pub fn add_word(&mut self, topic_id: usize, word: String, probability: f64, frequency: Option<u64>) -> PyResult<()> {
@@ -76,9 +79,9 @@ impl PyTopicModelBuilder {
             return Err(PyValueError::new_err("The probability has to be a normal number!"))
         }
         let word_id = self.voc.add_value(word);
-        self.set_probability(topic_id, word_id, probability);
+        self.set_probability_impl(topic_id, word_id, probability);
         if let Some(frequency) = frequency {
-            self.set_frequency(topic_id, frequency);
+            self.set_frequency_impl(topic_id, frequency);
         }
         Ok(())
     }
@@ -91,23 +94,37 @@ impl PyTopicModelBuilder {
         self.document_lengths = document_lengths;
     }
 
-    pub fn build(&self) -> PyResult<PyTopicModel> {
+    pub fn build(
+        &self,
+        unset_words_become_smallest: Option<bool>,
+        normalize: Option<bool>
+    ) -> PyResult<PyTopicModel> {
         let mut topics = self.topics.clone();
 
-        let iter = topics
-            .iter()
-            .flatten()
-            .cloned();
+        if topics.iter().flatten().any(|value| value.is_nan()) {
+            let min_value = topics
+                .iter()
+                .flatten()
+                .cloned()
+                .min_partial_filtered()
+                .ok_or_else(|| PyValueError::new_err("There has to be some probability given!"))?;
 
-        if iter.clone().any(|value| value.is_nan()) {
-            let min_value = iter
-                .min_partial()
-                .map_err(|err| PyValueError::new_err(err.to_string()))?
-                .ok_or_else(|| PyValueError::new_err("There has to be some probability given!"))? - f64::EPSILON;
+            let min_value = if unset_words_become_smallest.unwrap_or_default() {
+                min_value - f64::EPSILON
+            } else {
+                min_value
+            };
 
-            for value in topics.iter_mut().flatten() {
-                if value.is_nan() {
-                    *value = min_value
+            for topic in topics.iter_mut() {
+                for value in topic.iter_mut() {
+                    if value.is_nan() {
+                        *value = min_value
+                    }
+                }
+                if self.voc.len() > topic.len() {
+                    let target_len = self.voc.len() - topic.len();
+                    topic.reserve(target_len);
+                    topic.extend(repeat_n(min_value, target_len))
                 }
             }
         }
@@ -120,8 +137,30 @@ impl PyTopicModelBuilder {
             self.document_lengths.clone().unwrap_or_default(),
         );
 
-        model.normalize_in_place();
+        if normalize.unwrap_or_default() {
+            model.normalize_in_place()
+        }
 
         Ok(PyTopicModel::wrap(model))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::py::topic_model_builder::PyTopicModelBuilder;
+    use crate::topicmodel::topic_model::DisplayableTopicModel;
+
+    #[test]
+    fn can_create(){
+        let mut builder = PyTopicModelBuilder::new(None);
+        builder.add_word(0, "hello".to_string(), 1.0, None).unwrap();
+        builder.add_word(1, "hello".to_string(), 2.0, None).unwrap();
+        builder.add_word(0, "beer".to_string(), 2.0, None).unwrap();
+        builder.add_word(0, "vat".to_string(), 2.0, None).unwrap();
+        builder.add_word(1, "cat".to_string(), 2.0, None).unwrap();
+        builder.add_word(1, "beer".to_string(), 1.5, None).unwrap();
+        builder.add_word(1, "leech".to_string(), 0.9, None).unwrap();
+        let topic = builder.build(None, None).expect("ERR");
+        topic.show_10().unwrap();
     }
 }
