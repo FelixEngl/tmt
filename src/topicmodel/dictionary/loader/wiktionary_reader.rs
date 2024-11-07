a4d0c18f7c59a5dec8dd6d229704f94fcf6e24d1
Felix Engl <felix.engl@hotmail.com>
2024-11-07 19:19:28 +0100

    Before Exchanging topic model things

--- src/topicmodel/dictionary/loader/wiktionary_reader.rs
+++ src/topicmodel/dictionary/loader/wiktionary_reader.rs
@@ -1,7 +1,6 @@
 use std::path::Path;
 use serde::{Deserialize, Serialize};
-use serde::de::IgnoredAny;
-use crate::topicmodel::dictionary::word_infos::{Language, PartOfSpeech};
+use crate::topicmodel::dictionary::word_infos::{PartOfSpeech};
 
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -9,119 +8,155 @@ pub struct ExtractedWord {
     /// the word form
     word: String,
     /// part-of-speech, such as "noun", "verb", "adj", "adv", "pron", "determiner", "prep" (preposition), "postp" (postposition), and many others. The complete list of possible values returned by the package can be found in wiktextract.PARTS_OF_SPEECH.
-    pos: PartOfSpeech,
+    pos: String,
     /// name of the language this word belongs to (e.g., English)
     lang: String,
     /// Wiktionary language code corresponding to lang key (e.g., en)
     lang_code: String,
     /// list of word senses (dictionaries) for this word/part-of-speech (see below)
-    sensed: Vec<Sense>,
+    #[serde(default)]
+    senses: Vec<Sense>,
     /// list of inflected or alternative forms specified for the word (e.g., plural, comparative, superlative, roman script version).
     /// This is a list of dictionaries, where each dictionary has a form key and a tags key.
     /// The tags identify what type of form it is. It may also contain "ipa", "roman", and "source" fields.
     /// The form can be "-" when the word is marked as not having that form
     /// (some of those will be word-specific, while others are language-specific; post-processing
     /// can drop such forms when no word has a value for that tag combination).
+    #[serde(default)]
     forms: Vec<Form>,
-    /// list of dictionaries containing pronunciation, hyphenation, rhyming, and related information. Each dictionary may have a tags key containing tags that clarify what kind of form that entry is. Different types of information are stored in different fields: ipa is IPA pronunciation, enPR is enPR pronunciation, audio is name of sound file in Wikimedia commons.
-    #[serde(default, skip)]
+    /// list of dictionaries containing pronunciation, hyphenation, rhyming, and related information.
+    /// Each dictionary may have a tags key containing tags that clarify what kind of form that
+    /// entry is. Different types of information are stored in different fields: ipa is IPA
+    /// pronunciation, enPR is enPR pronunciation, audio is name of sound file in Wikimedia commons.
+    #[serde(default)]
     sounds: (),
     /// list of non-disambiguated categories for the word
+    #[serde(default)]
     categories: Vec<String>,
     /// list of non-disambiguated topics for the word
+    #[serde(default)]
     topics: Vec<String>,
     /// non-disambiguated translation entries (see below)
+    #[serde(default)]
     translations: Vec<Translation>,
     /// etymology section as cleaned text
-    #[serde(default, skip)]
+    #[serde(default)]
     etymology_text: (),
     /// templates and their arguments and expansions from the etymology section. These can be used to easily parse etymological relations. Certain common templates that do not signify etymological relations are not included.
-    #[serde(default, skip)]
+    #[serde(default)]
     etymology_templates: (),
     /// for words with multiple numbered etymologies, this contains the number of the etymology under which this entry appeared
-    #[serde(default, skip)]
+    #[serde(default)]
     etymology_number: (),
     /// descendants of the word (see below)
-    #[serde(default, skip)]
+    #[serde(default)]
     descendants: (),
     /// non-disambiguated synonym linkages for the word (see below)
+    #[serde(default)]
     synonyms: Vec<Linkage>,
     /// non-disambiguated antonym linkages for the word (see below)
+    #[serde(default)]
     antonyms: Vec<Linkage>,
     /// non-disambiguated hypernym linkages for the word (see below)
+    #[serde(default)]
     hypernyms: Vec<Linkage>,
     /// non-disambiguated linkages indicating being part of something (see below) (not systematically encoded)
+    #[serde(default)]
     holonyms: Vec<Linkage>,
     /// non-disambiguated linkages indicating having a part (see below) (fairly rare)
+    #[serde(default)]
     meronyms: Vec<Linkage>,
     /// non-disambiguated derived word linkages for the word (see below)
+    #[serde(default)]
     derived: Vec<Linkage>,
     /// non-disambiguated related word linkages for the word (see below)
+    #[serde(default)]
     related: Vec<Linkage>,
     /// non-disambiguated coordinate term linkages for the word (see below)
+    #[serde(default)]
     coordinate_terms: Vec<Linkage>,
     /// non-disambiguated Wikidata identifer
-    wikidata: String,
+    #[serde(default)]
+    wikidata: Option<String>,
     /// non-disambiguated page title in Wikipedia (possibly prefixed by language id)
-    wiktionary : String,
+    #[serde(default)]
+    wiktionary : Option<String>,
     /// part-of-speech specific head tags for the word. This basically just captures the templates (their name and arguments) as a list of dictionaries. Most applications may want to ignore this.
-    #[serde(default, skip)]
+    #[serde(default)]
     head_templates : (),
     /// conjugation and declension templates found for the word, as dictionaries. These basically capture the language-specific inflection template for the word. Note that for some languages inflection information is also contained in head_templates. XXX in the very near future, we will start parsing inflections from the inflection tables into forms, so there is usually no need to use the inflection_templates data.
-    #[serde(default, skip)]
+    #[serde(default)]
     inflection_templates: ()
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Sense {
     /// list of gloss strings for the word sense (usually only one). This has been cleaned, and should be straightforward text with no tagging.
+    #[serde(default)]
     glosses: Vec<String>,
     /// list of gloss strings for the word sense, with less cleaning than
+    #[serde(default)]
     raw_glosses: Vec<String>,
     /// list of qualifiers and tags for the gloss.
     /// This is a list of strings, and may include
     /// words such as "archaic", "colloquial", "present",
     /// "participle", "plural", "feminine", and many others
     /// (new words may appear arbitrarily).
+    #[serde(default)]
     tags: Vec<String>,
     /// list of sense-disambiguated category names extracted from (a subset) of the Category links on the page
+    #[serde(default)]
     categories: Vec<String>,
     /// list of sense-disambiguated topic names (kind of similar to categories but determined differently)
+    #[serde(default)]
     topics: Vec<String>,
     /// list of words that his sense is an alternative form of; this is a list of dictionaries, with field word containing the linked word and optionally extra containing additional text
-    #[serde(default, skip)]
+    #[serde(default)]
     alt_of: (),
     /// list of words that this sense is an inflected form of; this is a list of dictionaries, with field word containing the linked word and optionally extra containing additional text
-    #[serde(default, skip)]
+    #[serde(default)]
     form_of: (),
     /// sense-disambiguated translation entries (see below)
+    #[serde(default)]
     translations: Vec<Translation>,
     /// sense-disambiguated synonym linkages for the word (see below)
+    #[serde(default)]
     synonyms: Vec<Linkage>,
     /// sense-disambiguated antonym linkages for the word (see below)
+    #[serde(default)]
     antonyms: Vec<Linkage>,
     /// sense-disambiguated hypernym linkages for the word (see below)
+    #[serde(default)]
     hypernyms: Vec<Linkage>,
     /// sense-disambiguated linkages indicating being part of something (see below) (not systematically encoded)
+    #[serde(default)]
     holonyms: Vec<Linkage>,
     /// sense-disambiguated linkages indicating having a part (see below) (fairly rare)
+    #[serde(default)]
     meronyms: Vec<Linkage>,
     /// sense-disambiguated coordinate_terms linkages (see below)
+    #[serde(default)]
     coordinate_terms: Vec<Linkage>,
     /// sense-disambiguated derived word linkages for the word (see below)
+    #[serde(default)]
     derived: Vec<Linkage>,
     /// sense-disambiguated related word linkages for the word (see below)
+    #[serde(default)]
     related: Vec<Linkage>,
     /// list of textual identifiers collected for the sense. If there is a QID for the entry (e.g., Q123), those are stored in the wikidata field.
+    #[serde(default)]
     senseid: Vec<String>,
     /// list of QIDs (e.g., Q123) for the sense
+    #[serde(default)]
     wikidata: Vec<String>,
     /// list of Wikipedia page titles (with optional language code prefix)
+    #[serde(default)]
     wikipedia: Vec<String>,
     /// list of usage examples, each example being a dictionary with text field containing the example text, optional ref field containing a source reference, optional english field containing English translation, optional type field containing example type (currently example or quotation if present), optional roman field containing romanization (for some languages written in non-Latin scripts), and optional (rare) note field contains English-language parenthesized note from the beginning of a non-english example.
-    #[serde(default, skip)]
+    #[serde(default)]
     examples: (),
     /// if the word sense has a qualifier that could not be parsed, that qualifier is put in this field (rare). Most qualifiers are parsed into tags and/or topics. The gloss with the qualifier still present can be found in raw_glosses.
+    #[serde(default)]
     english: Vec<String>,
 }
 
@@ -130,27 +165,32 @@ pub struct Sense {
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Translation {
     /// optional alternative form of the translation (e.g., in a different script)
+    #[serde(default)]
     alt: Option<String>,
     /// Wiktionary's 2 or 3-letter language code for the language the translation is for
     code: String,
     /// English text, generally clarifying the target sense of the translation
-    #[serde(default, skip)]
+    #[serde(default)]
     english: (),
     /// the language name that the translation is for
     lang: String,
     /// optional text describing or commenting on the translation
+    #[serde(default)]
     note: Option<String>,
     /// optional romanization of the translation (when in non-Latin characters)
-    #[serde(default, skip)]
+    #[serde(default)]
     roman: (),
     /// optional sense indicating the meaning for which this is a translation (this is a free-text string, and may not match any gloss exactly)
-    #[serde(default, skip)]
+    #[serde(default)]
     sense: (),
     /// optional list of qualifiers for the translations, e.g., gender
+    #[serde(default)]
     tags: Vec<String>,
     /// optional taxonomic name of an organism mentioned in the translation
+    #[serde(default)]
     taxonomic: Option<String>,
     /// the translation in the specified language (may be missing when note is present)
+    #[serde(default)]
     word: Option<String>
 }
 
@@ -160,6 +200,7 @@ pub struct Translation {
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Linkage {
     /// optional alternative form of the target (e.g., in a different script)
+    #[serde(default)]
     alt: Option<String>,
     /// optional English text associated with the sense, usually identifying the linked target sense
     #[serde(default, skip)]
@@ -170,10 +211,13 @@ pub struct Linkage {
     /// text identifying the word sense or context (e.g., "to rain very heavily")
     sense: String,
     /// qualifiers specified for the sense (e.g., field of study, region, dialect, style)
+    #[serde(default)]
     tags: Vec<String>,
     /// optional taxonomic name associated with the linkage
+    #[serde(default)]
     taxonomic: Option<String>,
     /// list of topic descriptors for the linkage (e.g., military)
+    #[serde(default)]
     topics: Vec<String>,
     /// the word this links to (string)
     word: String
@@ -182,6 +226,7 @@ pub struct Linkage {
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Form {
     form: String,
+    #[serde(default)]
     tags: Vec<String>
 }
 
@@ -211,19 +256,30 @@ mod test {
             )
         );
 
-        let ct_err = reader.lines().par_bridge().map(|value| {
+        let ct_err = reader.lines().skip(50_000).par_bridge().map(|value| {
             if let Ok(value) = value {
-                if let Ok(value) = serde_json::from_str::<ExtractedWord>(&value) {
-                    (1, 0, 0)
-                } else {
-                    (0, 1, 0)
+
+                match serde_json::from_str::<ExtractedWord>(&value) {
+                    Ok(_) => {
+                        (1, 0, 0)
+                    }
+                    Err(value) => {
+                        panic!("{value:?}")
+                    }
                 }
+
+                // if let Ok(value) = serde_json::from_str::<ExtractedWord>(&value) {
+                //     (1, 0, 0)
+                // } else {
+                //     (0, 1, 0)
+                // }
             } else {
                 (0, 0, 1)
             }
-        }).reduce(||(0usize, 0usize, 0usize), |(a, b, c), (d, e, f)| {
-            (a+d, b+e, c+f)
-        });
+        }).reduce(
+            ||(0usize, 0usize, 0usize),
+            |(a, b, c), (d, e, f)| (a+d, b+e, c+f)
+        );
 
         println!("{:?}", ct_err);
 
